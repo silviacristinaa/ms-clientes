5d12f9e3cb46a6a219fba21cce5b521b5789bba5
silviacristinaa <silviacristinaalexandre1@gmail.com>
2023-04-02 09:37:59 -0300

    [feature/02] - Continuação implementação

--- src/main/java/com/github/silviacristinaa/msclientes/application/CustomerResource.java
+++ src/main/java/com/github/silviacristinaa/msclientes/application/CustomerResource.java
@@ -5,6 +5,7 @@ import com.github.silviacristinaa.msclientes.application.representation.Customer
 import com.github.silviacristinaa.msclientes.exceptions.ConflictException;
 import com.github.silviacristinaa.msclientes.exceptions.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -15,9 +16,16 @@ import java.net.URI;
 @RestController
 @RequestMapping("customers")
 @RequiredArgsConstructor
+@Slf4j
 public class CustomerResource {
 
     private final CustomerService customerService;
+
+    @GetMapping
+    public String status(){
+        log.info("Obtendo o status do microservice de clientes");
+        return "ok";
+    }
 
     @PostMapping
     public ResponseEntity save(@RequestBody @Valid CustomerSaveRequest request) throws ConflictException {
